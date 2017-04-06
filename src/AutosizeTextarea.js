2aa4ae197aa4c62c25a7f37df2d8ac3489fe45f5
Mason <mason@nerdinacan.com>
2017-04-06 12:04:28 -0600

    Initial commit

---
+++ src/AutosizeTextarea.js
@@ -0,0 +1,20 @@
+import autosize from 'autosize'
+
+export default AutosizeTextarea = {
+
+	props: ['value'],
+
+	template: `<textarea ref="textarea" @input="updateValue($event.target.value)">{{ value }}</textarea>`,
+
+	methods: {
+		updateValue(val) {
+			autosize(this.$el);
+			this.$emit('input', val);
+		}
+	},
+
+	mounted() {
+		autosize(this.$el)
+	}
+
+}
